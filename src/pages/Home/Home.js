79988b7e4ed774c2bba85c8509d52af424427d0a
Ashish <ashishsawant2150@gmail.com>
2023-03-06 10:05:59 +0530

    fixed horizontal scroll bar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./home.css";
 // import svgImg from "../../assets/img/perfil.png";
-import resume from "../../assets/pdf/Ashish-Cv.pdf"
-import { AnimationOnScroll } from 'react-animation-on-scroll';
+import resume from "../../assets/pdf/Ashish-Cv.pdf";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 const Home = () => {
   return (
@@ -41,58 +41,57 @@ const Home = () => {
               rel="noreferrer"
               className="home-social-icon"
             >
-            <i className="uil uil-file-download-alt"></i>
+              <i className="uil uil-file-download-alt"></i>
             </a>
           </div>
-          {/* <div className="home-img">
-              <svg
-                className="home-blob"
-                viewBox="0 0 200 187"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-              >
-                <mask id="mask0" mask-type="alpha">
-                  <path
-                    d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
-        130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
-        97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
-        0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
-                  />
-                </mask>
-                <g mask="url(#mask0)">
-                  <path
-                    d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
-        165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
-        129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
-        -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
-                  />
-                  <image
-                    className="home-blob-img"
-                    x="12"
-                    y="18"
-                    xlinkHref={svgImg}
-                  />
-                </g>
-              </svg>
-            </div> */}
           <div className="home-data">
             <h1 className="home-title">
-              Hi, I am <AnimationOnScroll animateIn="animate__zoomInDown" duration={1} style={{display:'inline-block'}} className="my-name">Ashish</AnimationOnScroll>{" "}
+              Hi, I am{" "}
+              <AnimationOnScroll 
+              style={{display:'inline-block'}}
+                animateIn="animate__zoomIn"
+                initiallyVisible={true}
+                animateOnce={true}
+                duration={1}
+              >
+                <span className="my-name">Ashish</span>
+              </AnimationOnScroll>{" "}
             </h1>
-            <AnimationOnScroll animateIn="animate__fadeInRight" delay={2000}  className="home-subtitle">Full Stack Developer</AnimationOnScroll>
-            <AnimationOnScroll animateIn="animate__fadeInRight" delay={3300} className="home-description">
-            A driven and problem-solving individual with a passion for full-stack development. Proficient in MERN stack technologies including MongoDB, Express, React, and Node.js, committed to delivering high-quality and scalable web applications.
+            <AnimationOnScroll
+              animateIn="animate__fadeInRight"
+              delay={2000}
+              animateOnce={true}
+              className="home-subtitle"
+            >
+              Full Stack Developer
             </AnimationOnScroll>
-            <AnimationOnScroll animateIn="animate__fadeInRight" delay={4300} href="#contact" className="btn btn-flex">
+            <AnimationOnScroll
+              animateIn="animate__fadeInRight"
+              delay={3300}
+              animateOnce={true}
+              className="home-description"
+            >
+              A driven and problem-solving individual with a passion for
+              full-stack development. Proficient in MERN stack technologies
+              including MongoDB, Express, React, and Node.js, committed to
+              delivering high-quality and scalable web applications.
+            </AnimationOnScroll>
+            <AnimationOnScroll
+              animateIn="animate__fadeInRight"
+              delay={4300}
+              animateOnce={true}
+              href="#contact"
+              className="btn btn-flex"
+            >
               Contact Me<i className="uil uil-message btn-icon"></i>
             </AnimationOnScroll>
           </div>
         </div>
-          <a href="#about" className="home-scroll-btn btn-flex ">
-            <i className="uil uil-mouse-alt home-scroll-mouse"></i>
-            <span className="home-scroll-name">Scroll down</span>
-            <i className="uil uil-arrow-down home-scroll-arrow"></i>
-          </a>
+        <a href="#about" className="home-scroll-btn btn-flex ">
+          <i className="uil uil-mouse-alt home-scroll-mouse"></i>
+          <span className="home-scroll-name">Scroll down</span>
+          <i className="uil uil-arrow-down home-scroll-arrow"></i>
+        </a>
       </div>
     </section>
   );
